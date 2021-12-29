a0cc1006ec818a0109dcc292d7ef621cb048a202
Sebastian-DHoore <sebas.gengar@gmail.com>
2021-12-29 17:30:03 +0100

    With and without hidden prints

--- Drinking_game.py
+++ Drinking_game.py
@@ -643,7 +643,7 @@ class DrinkingGame(object):
         self.turn(row, guess, side)
 
 
-# Simulation
+# Hidden simulation
 
 with HiddenPrints():
     while 1:
@@ -664,3 +664,24 @@ with HiddenPrints():
                 str(myGame.players[0].shots + myGame.players[1].shots
                     + myGame.players[2].shots + myGame.players[3].shots))
             filetowrite.write(", ")
+
+# Simulation
+
+# while 1:
+#     myGame = DrinkingGame()
+#     myGame.start()
+#     while not myGame.game_end:
+#         myGame.make_best_move()
+#     myGame.end()
+#     file1 = 'Drinking_game_data/Turns.txt'
+#     file2 = 'Drinking_game_data/Shots.txt'
+#     with open(file1, 'a') as filetowrite:
+#         filetowrite.write(
+#             str(myGame.players[0].turns + myGame.players[1].turns
+#                 + myGame.players[2].turns + myGame.players[3].turns))
+#         filetowrite.write(", ")
+#     with open(file2, 'a') as filetowrite:
+#         filetowrite.write(
+#             str(myGame.players[0].shots + myGame.players[1].shots
+#                 + myGame.players[2].shots + myGame.players[3].shots))
+#         filetowrite.write(", ")
